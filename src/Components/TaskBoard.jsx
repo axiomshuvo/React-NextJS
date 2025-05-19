ba3a1ad69c776718f5b00eff220491e2da0b2c05
Pradipta Sarker <techshuvo@gmail.com>
2025-05-20 01:15:17 +0600

    popup modal add for input list

--- src/Components/TaskBoard.jsx
+++ src/Components/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTaskModal from "./task/AddTaskModal";
 import SearchTask from "./task/SearchTask";
 import TaskAction from "./task/TaskAction";
 import TaskList from "./task/TaskList";
@@ -11,20 +12,26 @@ export default function TaskBoard() {
       "I want to Learn React such that I can treat it like my slave and make it do whatever I want to do",
     tags: ["web", "react", "js"],
     priority: "High",
-    isFavorite: true,
+    isFavorite: false,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask() {
+    console.log("adding a task");
+  }
 
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal />}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction />
+            <TaskAction onAddClick={() => setShowAddModal(true)} />
             <TaskList tasks={tasks} />
           </div>
         </div>
