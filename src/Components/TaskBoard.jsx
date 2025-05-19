e48094cf4c32edbabb20b5cf9fb45df8027378c3
Pradipta Sarker <techshuvo@gmail.com>
2025-05-19 17:58:22 +0600

    state update and tasklist ui update with that

--- src/Components/TaskBoard.jsx
+++ src/Components/TaskBoard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import SearchTask from "./task/SearchTask";
 import TaskAction from "./task/TaskAction";
 import TaskList from "./task/TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to Learn React such that I can treat it like my slave and make it do whatever I want to do",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <>
       <section className="mb-20" id="tasks">
@@ -13,7 +25,7 @@ export default function TaskBoard() {
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
             <TaskAction />
-            <TaskList />
+            <TaskList tasks={tasks} />
           </div>
         </div>
       </section>
