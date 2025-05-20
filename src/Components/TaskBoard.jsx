26b39ef83e5cc0743d201b155ba8a223c4fcd6aa
Pradipta Sarker <techshuvo@gmail.com>
2025-05-20 06:07:05 +0600

    add modal functionality, list functionality - add, delete, edit,fav

--- src/Components/TaskBoard.jsx
+++ src/Components/TaskBoard.jsx
@@ -16,23 +16,81 @@ export default function TaskBoard() {
   };
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-  function handleAddTask() {
-    console.log("adding a task");
+  function handleAddEditTask(newTask, isAdd) {
+    // console.log("adding a task", newTask);
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  }
+  function handleEditTask(task) {
+    setShowAddModal(true);
+    setTaskToUpdate(task);
+
+    console.log(task);
+  }
+
+  function handleDeleteTask(taskId) {
+    const taskAfterDelete = tasks.filter((task) => task.id !== taskId);
+    setTasks(taskAfterDelete);
+  }
+
+  function handleCloseClick() {
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  }
+
+  function handleAllDeleteClick() {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+
+  function handleFav(taskId) {
+    const taskIndex = tasks.findIndex((task) => task.id == taskId);
+    const newTasks = [...tasks];
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
   }
 
   return (
     <>
       <section className="mb-20" id="tasks">
-        {showAddModal && <AddTaskModal />}
+        {showAddModal && (
+          <AddTaskModal
+            onSave={handleAddEditTask}
+            taskToUpdate={taskToUpdate}
+            onCloseClick={handleCloseClick}
+          />
+        )}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction onAddClick={() => setShowAddModal(true)} />
-            <TaskList tasks={tasks} />
+            <TaskAction
+              onAddClick={() => setShowAddModal(true)}
+              onDeleteAllClick={handleAllDeleteClick}
+            />
+            <TaskList
+              tasks={tasks}
+              onEdit={handleEditTask}
+              onDelete={handleDeleteTask}
+              onFav={handleFav}
+            />
           </div>
         </div>
       </section>
--- src/Components/task/AddTaskModal.jsx
+++ src/Components/task/AddTaskModal.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 
-export default function AddTaskModal() {
-  const [Task, setTask] = useState({
-    // id: crypto.randomUUID(),
-    title: "",
-    description: "",
-    tags: [],
-    priority: "",
-    isFavorite: false,
-  });
+export default function AddTaskModal({ onSave, taskToUpdate, onCloseClick }) {
+  const [task, setTask] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavorite: false,
+    }
+  );
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    let value = e.target.value;
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
+
+  const handleChange = (evt) => {
+    const name = evt.target.name;
+    let value = evt.target.value;
     if (name === "tags") {
       value = value.split(",");
     }
 
     setTask({
-      ...Task,
+      ...task,
       [name]: value,
     });
-    console.log(Task);
+    console.log(task);
   };
 
   return (
@@ -30,7 +34,7 @@ export default function AddTaskModal() {
 
       <form className="z-99 absolute top-1/5 left-1/4 mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {isAdd ? "Add New Task" : "Edit Task"}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -42,7 +46,7 @@ export default function AddTaskModal() {
               name="title"
               id="title"
               required
-              value={Task.title}
+              value={task.title}
               onChange={handleChange}
             />
           </div>
@@ -55,7 +59,7 @@ export default function AddTaskModal() {
               name="description"
               id="description"
               required
-              Value={Task.description}
+              Value={task.description}
               onChange={handleChange}
             />
           </div>
@@ -69,7 +73,7 @@ export default function AddTaskModal() {
                 name="tags"
                 id="tags"
                 required
-                Value={Task.tags}
+                Value={task.tags}
                 onChange={handleChange}
               />
             </div>
@@ -81,24 +85,32 @@ export default function AddTaskModal() {
                 name="priority"
                 id="priority"
                 required
-                Value={Task.priority}
+                Value={task.priority}
                 onChange={handleChange}
               >
                 <option value="">Select Priority</option>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
               </select>
             </div>
           </div>
         </div>
 
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between lg:mt-20">
+          <button
+            className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={onCloseClick}
+          >
+            Close
+          </button>
+
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={() => onSave(task, isAdd)}
           >
-            Create new Task
+            Save
           </button>
         </div>
       </form>
